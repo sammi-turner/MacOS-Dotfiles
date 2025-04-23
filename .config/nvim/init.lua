7c06b3b408031940b278530be6a0b1bafdb1319d
Sammi Turner <ms.sammi.turner@gmail.com>
2025-04-24 00:13:00 +0100

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,8 +11,8 @@ vim.o.termguicolors = true
 vim.wo.number = true
 
 -- Set tab width to 4 spaces
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 -- Enable bracket completion
