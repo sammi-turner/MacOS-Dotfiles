9013f3fe934c0dc743d471e4b72557193df7d3c1
Sammi Turner <ms.sammi.turner@gmail.com>
2025-04-29 20:55:59 +0100

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -20,22 +20,22 @@ return {
     bottom = 5,
   },
 
-  -- Pane-focused keybinds (no tab operations)
+  -- Disable default keybinds (as per your config)
   disable_default_key_bindings = true,
   keys = {
     -- Pane Creation ------------------------------------------------------
-    { key = "|", mods = "CTRL|SHIFT", action = act.SplitVertical { domain = "CurrentPaneDomain" } },    -- Vertical split (Shift+\)
+    { key = "|", mods = "CTRL|SHIFT", action = act.SplitVertical { domain = "CurrentPaneDomain" } }, -- Vertical split (Shift+\)
     { key = "_", mods = "CTRL|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } }, -- Horizontal split (Shift+-)
 
     -- Pane Deletion ------------------------------------------------------
-    { key = "x", mods = "CTRL|SHIFT", action = act.CloseCurrentPane { confirm = true } },             -- Close current pane
-    { key = "X", mods = "CTRL|SHIFT", action = act.CloseCurrentPane { confirm = false } },            -- Force close pane
+    { key = "x", mods = "CTRL|SHIFT", action = act.CloseCurrentPane { confirm = true } }, -- Close current pane
+    { key = "X", mods = "CTRL|SHIFT", action = act.CloseCurrentPane { confirm = false } }, -- Force close pane (no confirm)
 
     -- Pane Navigation ----------------------------------------------------
-    { key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Left" },                    -- Left pane
-    { key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Down" },                    -- Down pane
-    { key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Up" },                     -- Up pane
-    { key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Right" },                   -- Right pane
+    { key = "h", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Left" }, -- Left pane
+    { key = "j", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Down" }, -- Down pane
+    { key = "k", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Up" }, -- Up pane
+    { key = "l", mods = "CTRL|SHIFT", action = act.ActivatePaneDirection "Right" }, -- Right pane
 
     -- Pane Resizing ------------------------------------------------------
     { key = "LeftArrow",  mods = "CTRL|SHIFT", action = act.AdjustPaneSize { "Left", 1 } },
@@ -44,12 +44,17 @@ return {
     { key = "DownArrow",  mods = "CTRL|SHIFT", action = act.AdjustPaneSize { "Down", 1 } },
 
     -- Pane Zoom/Focus ----------------------------------------------------
-    { key = "z", mods = "CTRL|SHIFT", action = act.TogglePaneZoomState },                             -- Zoom/unzoom pane
-    { key = "f", mods = "CTRL|SHIFT", action = act.ToggleFullScreen },                               -- Fullscreen current pane
+    { key = "z", mods = "CTRL|SHIFT", action = act.TogglePaneZoomState }, -- Zoom/unzoom pane
+    { key = "f", mods = "CTRL|SHIFT", action = act.ToggleFullScreen }, -- Fullscreen current pane
 
     -- Essential Operations -----------------------------------------------
-    { key = "r", mods = "CTRL|SHIFT", action = act.ReloadConfiguration },                            -- Reload config
-    { key = "c", mods = "CTRL|SHIFT", action = act.CopyTo "Clipboard" },                             -- Copy
-    { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom "Clipboard" },                          -- Paste
+    { key = "r", mods = "CTRL|SHIFT", action = act.ReloadConfiguration }, -- Reload config
+    { key = "c", mods = "CTRL|SHIFT", action = act.CopyTo "Clipboard" }, -- Copy
+    { key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom "Clipboard" }, -- Paste
+
+    -- Font Size Adjustments (New Additions) ------------------------------
+    { key = "[", mods = "CTRL|SHIFT", action = act.DecreaseFontSize }, -- Decrease font size (CTRL+SHIFT+[)
+    { key = "]", mods = "CTRL|SHIFT", action = act.IncreaseFontSize }, -- Increase font size (CTRL+SHIFT+])
+    { key = "0", mods = "CTRL|SHIFT", action = act.ResetFontSize }, -- Reset font size (CTRL+SHIFT+0)
   }
 }
